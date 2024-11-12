ea97214d3db8b17c624712ef29eaf4e85bb77184
chandrika <chandrikaaws55@gmail.com>
2024-11-12 12:05:52 +0530

    security group

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "project_name"{
+
+}
+variable "environment"{
+
+}
+variable "sg_name"{
+
+}
+variable "sg_tags"{
+    default = {}
+}
+variable "common_tags"{
+    default = {}
+}
+variable "vpc_id"{
+    
+}
